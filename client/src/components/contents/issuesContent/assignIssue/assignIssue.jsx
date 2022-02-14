38461f27822a3dddbaf6c9290aec21bc0668376d
Ahmad Salameh <ahmadsalameh.dev@gmail.com>
2022-02-14 11:05:40 +0200

    Implement loading for assignIssue popup

--- client/src/components/contents/issuesContent/assignIssue/assignIssue.jsx
+++ client/src/components/contents/issuesContent/assignIssue/assignIssue.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { UserContext } from "../../../../context/userContext";
 import Assignee from "./assignee/assignee";
 import teamsService from "../../../../services/teamsService";
+import LoadingSpinner from "../../../common/loadingSpinner/loadingSpinner";
 
 import "./assignIssue.css";
 
@@ -9,14 +10,17 @@ const AssignIssue = ({ bugs, bug, setBugs, setPopup }) => {
   const { user, token } = useContext(UserContext);
 
   const [teams, setTeams] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     teamsService.setToken(token);
+    setLoading(true);
     const getMembers = async () => {
       try {
         const { data } = await teamsService.getMembers(bug.app);
         setTeams(data);
       } catch (ex) {}
+      setLoading(false);
     };
     getMembers();
   }, [token]);
@@ -28,6 +32,13 @@ const AssignIssue = ({ bugs, bug, setBugs, setPopup }) => {
     setBugs: setBugs,
     setPopup: setPopup,
   };
+
+  if (loading)
+    return (
+      <div className="assign-issue-loading">
+        <LoadingSpinner classes="loading-spinner-icon" />
+      </div>
+    );
 
   return (
     <div className="assign-issue-members">
