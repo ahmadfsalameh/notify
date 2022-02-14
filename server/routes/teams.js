44efbb787c4f3f0a766eaeb9d7b1f3fdf840c1f3
Ahmad Salameh <ahmadsalameh.dev@gmail.com>
2022-02-14 09:49:11 +0200

    Fix conflict

--- server/routes/teams.js
+++ server/routes/teams.js
@@ -5,10 +5,7 @@ import { getTeams, createTeam, deleteTeam } from "../controllers/teams.js";
 const router = express.Router();
 
 router.get("/", auth, getTeams);
-<<<<<<< HEAD
 router.get("/members/appId", auth, getMembers);
-=======
->>>>>>> 6417656aff97e01c69c98ea5d1f8f9921703201e
 router.post("/", auth, createTeam);
 router.delete("/:id", auth, deleteTeam);
 
