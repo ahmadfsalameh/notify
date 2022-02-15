65981bedcc02e51d61dc3c237e6d464d4f2e64ef
Ahmad Salameh <ahmadsalameh.dev@gmail.com>
2022-02-15 07:45:53 +0200

    Implement settings contents component

--- client/src/components/contents/settingsContent/settingsContent.jsx
+++ client/src/components/contents/settingsContent/settingsContent.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../../../context/userContext";
+import Button from "../../common/button/button";
+import text from "../../../constants/text.json";
+
+import "./settingsContent.css";
 
 const SettingsContent = () => {
-  return <div className="content-container-main">Settings</div>;
+  const { user } = useContext(UserContext);
+  const { settings } = text;
+  return (
+    <section className="settings">
+      <div className="settings-container">
+        <ul>
+          <li>
+            <p>
+              <span>{settings.name}</span>
+              {user.name}
+            </p>
+            <Button label={settings.change} className="btn-action" />
+          </li>
+          <li>
+            <p>
+              <span>{settings.password}</span>
+              ••••••••
+            </p>
+            <Button label={settings.change} className="btn-action" />
+          </li>
+          <li>
+            <p>
+              <span>{settings.account}</span>
+              {settings.danger}
+            </p>
+            <Button label={settings.delete} className="btn-action btn-danger" />
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default SettingsContent;
