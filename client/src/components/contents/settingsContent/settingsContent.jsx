87ddd9eb35840d953f22c10c112472fa888ce56a
Ahmad Salameh <ahmadsalameh.dev@gmail.com>
2022-02-15 07:09:14 +0200

    Create settings content component

---
+++ client/src/components/contents/settingsContent/settingsContent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SettingsContent = () => {
+  return <div className="content-container-main">Settings</div>;
+};
+
+export default SettingsContent;
--- client/src/layouts/settings/settings.jsx
+++ client/src/layouts/settings/settings.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import DashboardStructure from "../../components/dashboardStructure/dashboardStructure";
+import SettingsContent from "../../components/contents/settingsContent/settingsContent";
 
 const Settings = () => {
-  return <DashboardStructure content={<p>Settings</p>} />;
+  return <DashboardStructure content={<SettingsContent />} />;
 };
 
 export default Settings;
